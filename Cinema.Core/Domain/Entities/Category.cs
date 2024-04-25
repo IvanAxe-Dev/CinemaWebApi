201d4f5e63459df46a3d8a59f74a3dc5b179da0e
Rosnovskyi Yaroslav <rosn.yaroslav@gmail.com>
2024-04-25 05:39:09 +0300

    clean architecture deployed, database connected

---
+++ Cinema.Core/Domain/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace Cinema.Core.Domain.Entities;
+
+public partial class Category
+{
+    public Guid Id { get; set; }
+
+    public string? Name { get; set; }
+}
---
+++ Cinema.Infrastructure/DatabaseContext/CinemaDbContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using Cinema.Core.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cinema.Infrastructure.DatabaseContext;
+
+public partial class CinemaDbContext : DbContext
+{
+    public CinemaDbContext()
+    {
+    }
+
+    public CinemaDbContext(DbContextOptions<CinemaDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    public virtual DbSet<MovieCategory> MovieCategories { get; set; }
+
+    public virtual DbSet<Session> Sessions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=DESKTOP-P3UPN32\\SQLEXPRESS01; Database=CinemaDB; Trusted_Connection=True; TrustServerCertificate=True; ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Movie>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.Director).HasMaxLength(50);
+            entity.Property(e => e.Image).HasColumnType("image");
+            entity.Property(e => e.ReleaseDate).HasColumnType("datetime");
+            entity.Property(e => e.Title).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<MovieCategory>(entity =>
+        {
+            entity.HasNoKey();
+
+            entity.HasOne(d => d.Category).WithMany()
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_MovieCategories_Categories");
+
+            entity.HasOne(d => d.Movie).WithMany()
+                .HasForeignKey(d => d.MovieId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_MovieCategories_Movies");
+        });
+
+        modelBuilder.Entity<Session>(entity =>
+        {
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.EndDate).HasColumnType("datetime");
+            entity.Property(e => e.Graphics).HasMaxLength(2);
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Privilege).HasMaxLength(50);
+            entity.Property(e => e.StartDate).HasColumnType("datetime");
+
+            entity.HasOne(d => d.Movie).WithMany(p => p.Sessions)
+                .HasForeignKey(d => d.MovieId)
+                .HasConstraintName("FK_Sessions_Movies");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
