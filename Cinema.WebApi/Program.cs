232e773b6be70885cb083bff6f0c4d7152cd4de5
Rostyslav <rostikboss2005@gmail.com>
2024-04-26 00:11:50 +0300

    logger connected

--- Cinema.WebApi/Program.cs
+++ Cinema.WebApi/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
 
 builder.Services.AddDbContext<SqlcinemadbContext>(options =>
 {
