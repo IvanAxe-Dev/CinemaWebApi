699f2ce5f4d5cf0319628568f12a69464c4507aa
IvanAxe-Dev <101930216+IvanAxe-Dev@users.noreply.github.com>
2024-04-27 16:38:37 +0300

    models, database updated

---
+++ Cinema.Core/Domain/Entities/CinemaHall.cs
@@ -0,0 +1,15 @@
+﻿using Cinema.Core.Enums;
+
+namespace Cinema.Core.Domain.Entities
+{
+    public partial class CinemaHall : BaseEntity
+    {
+        public Graphics? Graphics { get; set; }
+
+        public Privilege? Privilege { get; set; }
+
+        public byte[][] Seats { get; set; }
+
+        public ICollection<Session> Sessions { get; set; } = new List<Session>();
+    }
+}
---
+++ Cinema.Core/Domain/Entities/Ticket.cs
@@ -0,0 +1,15 @@
+﻿namespace Cinema.Core.Domain.Entities
+{
+    public partial class Ticket : BaseEntity
+    {
+        public int SeatRow { get; set; }
+
+        public int SeatNumber { get; set; }
+
+        //public Guid UserId { get; set; }
+
+        public Guid SessionId { get; set; }
+
+        public Session Session { get; set; }
+    }
+}
