7c051dc929ab9dd04b8e93956523c4a760b73e9e
IvanAxe-Dev <101930216+IvanAxe-Dev@users.noreply.github.com>
2024-05-12 13:23:59 +0300

    explanations initiated

--- Cinema.WebApi/Controllers/TicketController.cs
+++ Cinema.WebApi/Controllers/TicketController.cs
@@ -2,10 +2,12 @@ using Cinema.Core.Domain.Entities;
 using Cinema.Core.DTO;
 using Cinema.Core.ServiceContracts;
 using MapsterMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cinema.WebApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class TicketController : ControllerBase
@@ -19,16 +21,28 @@ namespace Cinema.WebApi.Controllers
             _mapster = mapster;
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpGet]
-        public async Task<ActionResult<List<Ticket>>> GetAll()
+        public async Task<ActionResult<List<Ticket>>> GetAllForAdmin()
         {
             List<Ticket> tickets = await _ticketService.GetAllAsync();
 
             return Ok(tickets);
         }
 
+        //add user identification as in [movie]
+        [Authorize(Roles = "User")]
+        [HttpGet]
+        public async Task<ActionResult<List<Ticket>>> GetAllForUser()
+        {
+            List<Ticket> tickets = await _ticketService.GetAllAsync();
+
+            return Ok(tickets);
+        }
+
+        [Authorize(Roles = "Admin")]
         [HttpGet("{id:guid}")]
-        public async Task<ActionResult<Ticket>> GetById(Guid id)
+        public async Task<ActionResult<Ticket>> GetAllForAdmin(Guid id)
         {
             Ticket? ticket = await _ticketService.FindByIdAsync(id);
 
@@ -40,14 +54,32 @@ namespace Cinema.WebApi.Controllers
             return Ok(ticket);
         }
 
+        //add user identification check as in [movie]
+        [Authorize(Roles = "User")]
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<Ticket>> GetByIdForUser(Guid id)
+        {
+            Ticket? ticket = await _ticketService.FindByIdAsync(id);
+
+            if (ticket == null)
+            {
+                return NotFound("Ticket not found");
+            }
+
+            return Ok(ticket);
+        }
+
+        //add user identification
         [HttpPost]
         public async Task<ActionResult<Ticket>> Create(TicketDto ticketDto)
         {
             Ticket newTicket = await _ticketService.Insert(_mapster.Map<Ticket>(ticketDto));
 
-            return CreatedAtAction(nameof(GetById), new { id = newTicket.Id }, newTicket);
+            return CreatedAtAction(nameof(GetByIdForUser), new { id = newTicket.Id }, newTicket);
         }
 
+        //add user identification
+        [Authorize(Roles = "Admin")]
         [HttpPut("{id:guid}")]
         public async Task<ActionResult<Ticket>> Update(Guid id, TicketDto ticketDto)
         {
@@ -62,7 +94,8 @@ namespace Cinema.WebApi.Controllers
 
             return Ok(await _ticketService.Update(ticket));
         }
-        
+
+        [Authorize(Roles = "Admin")]
         [HttpDelete("{id:guid}")]
         public async Task<ActionResult> Delete(Guid id)
         {
