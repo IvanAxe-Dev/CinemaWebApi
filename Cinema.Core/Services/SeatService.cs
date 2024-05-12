93d00f05bc2fcff746529063cd027addf48be1dd
Rosikss <76153577+Rosikss@users.noreply.github.com>
2024-05-12 19:08:14 +0300

    Session refactoring (#19)
    * Session Create method refactored, seats service update
    
    * DTO fix

--- Cinema.Core/Services/SeatService.cs
+++ Cinema.Core/Services/SeatService.cs
@@ -1,5 +1,6 @@
 ﻿using Cinema.Core.Domain.Entities;
 using Cinema.Core.Domain.RepositoryContracts;
+using Cinema.Core.DTO;
 using Cinema.Core.ServiceContracts;
 using MapsterMapper;
 
@@ -8,10 +9,34 @@ namespace Cinema.Core.Services
     public class SeatService : Service<Seat>, ISeatService
     {
         private readonly IMapper _mapster;
+        private readonly ISeatRepository _seatRepository;
+        private readonly ISessionService _sessionService;
 
-        public SeatService(ISeatRepository repository, IMapper mapster) : base(repository)
+        public SeatService(ISeatRepository repository, IMapper mapster, ISeatRepository seatRepository, ISessionService sessionService) : base(repository)
         {
             _mapster = mapster;
+            _seatRepository = seatRepository;
+            _sessionService = sessionService;
+        }
+
+        public async Task<List<SeatResponse>> CreateSeatsForSession(Guid sessionId)
+        {
+            var session = await _sessionService.FindByIdAsync(sessionId);
+
+            List<Seat> seats = new List<Seat>();    
+
+            for (int row = 1; row <= session.CinemaHall.RowsCount; row++)
+            {
+                for (int number = 1; number <= session.CinemaHall.NumbersCount; number++)
+                {
+                    seats.Add(new Seat { Row = row, Number = number, SessionId = session.Id });
+                }
+            }
+
+            await _seatRepository.PostRange(seats);
+            await _seatRepository.SaveChangesAsync();
+
+            return _mapster.Map<List<SeatResponse>>(seats); ;
         }
 
     }
