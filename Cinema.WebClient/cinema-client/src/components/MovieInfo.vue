7753770d3c985bb487b845017d7d3c6a05929f09
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-05-17 18:12:39 +0300

    created movie info page layout

---
+++ Cinema.WebClient/cinema-client/src/components/MovieInfo.vue
@@ -0,0 +1,103 @@
+<script setup>
+import { defineProps, computed } from 'vue';
+import { formatDate, getAverageRating } from '../utils.js';
+
+const props = defineProps({
+  movie: {
+    type: Object,
+    required: true
+  }
+});
+
+const releaseYear = computed(() => {
+  const date = new Date(props.movie.releaseDate);
+  return date.getFullYear();
+});
+
+const rentalTerm = computed(() => {
+  const startDate = formatDate(props.movie.rentalStartDate);
+  const endDate = formatDate(props.movie.rentalEndDate);
+  return startDate === endDate ? startDate : `${startDate} - ${endDate}`;
+});
+
+const rating = computed(() => {
+  return getAverageRating(props.movie.ratings);
+});
+
+const genres = computed(() => {
+  const genresArray = [];
+  for (const category of props.movie.categories) {
+    genresArray.push(category.name);
+  }
+  return genresArray.join(', ');
+});
+</script>
+
+<template>
+  <div class="movie-info">
+    <h1>{{ props.movie.title }}</h1>
+    <div class="info-content">
+      <div class="labels">
+        <div class="label">Age:</div>
+        <div class="label">Year:</div>
+        <div class="label">Title:</div>
+        <div class="label">Director:</div>
+        <div class="label">Rental term:</div>
+        <div class="label">Ranking:</div>
+        <div class="label">Genre:</div>
+      </div>
+      <div class="values">
+        <div class="value">{{ props.movie.ageRestriction }}</div>
+        <div class="value">{{ releaseYear }}</div>
+        <div class="value">{{ props.movie.title }}</div>
+        <div class="value">{{ props.movie.director }}</div>
+        <div class="value">{{ rentalTerm }}</div>
+        <div class="value">{{ rating }}</div>
+        <div class="value">{{ genres }}</div>
+      </div>
+    </div>
+    <div class="movie-description">
+      <span class="description">{{ props.movie.description }}</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.movie-info {
+  text-align: left;
+}
+
+.blank-space {
+  background-color: rgb(62, 96, 125);
+  width: 300px;
+  height: 100%;
+}
+
+.info-content {
+  max-width: 400px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.labels, .values {
+  flex: 1;
+}
+
+.label, .value {
+  margin-bottom: 10px;
+}
+
+.label {
+  font-weight: bold;
+  color: #454545;
+}
+
+.value {
+  font-weight:400;
+}
+
+.movie-description {
+  margin-top: 20px;
+  white-space: pre;
+}
+</style>
--- Cinema.WebClient/cinema-client/src/views/MovieDetailsView.vue
+++ Cinema.WebClient/cinema-client/src/views/MovieDetailsView.vue
@@ -1,5 +1,158 @@
+<script setup>
+import MovieInfo from '../components/MovieInfo.vue';
+
+const movie = {
+  id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+  title: "The Dark Knight",
+  rentalStartDate: "2024-05-15T07:09:52.280Z",
+  rentalEndDate: "2024-05-25T07:09:52.280Z",
+  description: "string",
+  imageUrl: "https://www.movieposters.com/cdn/shop/files/darkknight.building.24x36_20e90057-f673-4cc3-9ce7-7b0d3eeb7d83_480x.progressive.jpg?v=1707491191",
+  releaseDate: "2024-05-15T07:09:52.280Z",
+  director: "Christopher Nolan",
+  duration: "string",
+  ageRestriction: 12,
+  trailerUrl: "string",
+  actors: "string",
+  ratings: [
+    6,
+    7,
+    10,
+    9,
+    8
+  ],
+  sessions: [
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "date": "string",
+      "startTime": "10:00",
+      "price": 100,
+      "availableSeats": 1,
+      "movieId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "cinemaHallId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    },
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "date": "string",
+      "startTime": "12:00",
+      "price": 140,
+      "availableSeats": 1,
+      "movieId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "cinemaHallId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    },
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "date": "string",
+      "startTime": "14:30",
+      "price": 160,
+      "availableSeats": 1,
+      "movieId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "cinemaHallId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    },
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "date": "string",
+      "startTime": "16:45",
+      "price": 100,
+      "availableSeats": 1,
+      "movieId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "cinemaHallId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    },
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "date": "string",
+      "startTime": "19:00",
+      "price": 80,
+      "availableSeats": 1,
+      "movieId": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "cinemaHallId": "3fa85f64-5717-4562-b3fc-2c963f66afa6"
+    }
+  ],
+  categories: [
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "name": "action"
+    },
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "name": "drama"
+    },
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "name": "fiction"
+    },
+    {
+      "id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+      "name": "action"
+    },
+  ]
+}
+
+</script>
+
 <template>
   <div class="movie-details">
-    <h1>This is details page for movie with id: {{ $route.params.id }}</h1>
+    <div class="poster-container">
+      <img :src="movie.imageUrl">
+      <button>Watch trailer</button>
+    </div>
+    <movie-info :movie="movie"></movie-info>
   </div>
 </template>
+
+<style scoped>
+.movie-details {
+  display: flex;
+  align-items: flex-start;
+  max-width: 1600px;
+  padding: 20px;
+  margin: 0 auto;
+}
+
+.poster-container {
+  position: fixed;
+  left: 40px;
+  width: 300px;
+  height: 600px;
+  border-radius: 15px;
+
+  img {
+    width: 100%;
+    height: auto;
+    border-radius: 15px;
+  }
+
+  button {
+    padding: 10px 20px;
+    border-width: 2px;
+    border-radius: 15px;
+    color: white;
+    background-color: #20232a;
+  }
+}
+
+.movie-info {
+  flex: 1;
+  margin-left: 360px;
+  padding: 20px;
+}
+
+@media (max-width: 768px) {
+  .movie-details {
+    flex-direction: column;
+  }
+  .poster-container {
+    position: static;
+    margin-bottom: 20px;
+    width: 100%;
+
+    img {
+      height: 100%;
+      width: auto;
+    }
+  }
+  .movie-info {
+    margin-left: 0;
+  }
+}
+</style>
