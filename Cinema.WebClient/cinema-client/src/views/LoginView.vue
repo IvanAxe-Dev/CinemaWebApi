9defe43ced23fcfa840ab16c79ab14a52b8d79c5
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-05-19 12:38:50 +0300

    format code: script tag at the top, remove duplicate tabs

--- Cinema.WebClient/cinema-client/src/views/LoginView.vue
+++ Cinema.WebClient/cinema-client/src/views/LoginView.vue
@@ -1,3 +1,43 @@
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            emailOrUsername: '',
+            password: ''
+        };
+    },
+    methods: {
+        login() {
+
+
+
+            const userData = {
+                emailOrUsername: this.emailOrUsername,
+                password: this.password
+            };
+
+            console.log(userData);
+            // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ POST-пїЅпїЅпїЅпїЅпїЅпїЅ
+            axios({
+                method: 'post',
+                url: 'api/Account/login',
+                data: {
+                    emailOrUsername: userData.emailOrUsername,
+                    password: userData.password
+                }
+            }).then(response => {
+                console.log(response);
+            }).catch(error => {
+                console.error(error);
+            });
+
+
+        }
+    }
+};
+</script>
+
 <template>
     <div>
         <div class="image-container">
@@ -15,198 +55,155 @@
                 <input type="password" placeholder="Password" id="password" v-model="password">
 
                 <button type="submit">Log In</button>
-                
+
             </form>
         </div>
     </div>
 </template>
 
-<script>
-    import axios from 'axios';
-    export default {
-        data() {
-            return {
-                emailOrUsername: '',
-                password: ''
-            };
-        },
-        methods: {
-            login() {
-
-
-
-                const userData = {
-                    emailOrUsername: this.emailOrUsername,
-                    password: this.password
-                };
-                
-                console.log(userData);
-                // Отправить POST-запрос
-                axios({
-                    method: 'post',
-                    url: 'api/Account/login',
-                    data: {
-                        emailOrUsername: userData.emailOrUsername,
-                        password: userData.password
-                    }
-                }).then(response => {
-                    console.log(response); 
-                }).catch(error => {
-                    console.error(error); 
-                });
-                
-
-            }
-        }
-    };
-</script>
-
 <style scoped>
-    *,
-    *:before,
-    *:after {
-        padding: 0;
-        margin: 0;
-        box-sizing: border-box;
-    }
+*,
+*:before,
+*:after {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
 
-    body {
-        background-color: #080710;
-        font-family: 'Poppins', sans-serif;
-        color: #ffffff;
-    }
+body {
+    background-color: #080710;
+    font-family: 'Poppins', sans-serif;
+    color: #ffffff;
+}
 
-    .background {
-        width: 430px;
-        height: 520px;
-        position: absolute;
-        transform: translate(-50%, -50%);
-        left: 50%;
-        top: 50%;
-    }
+.background {
+    width: 430px;
+    height: 520px;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    left: 50%;
+    top: 50%;
+}
 
-        .background .shape {
-            height: 200px;
-            width: 200px;
-            position: absolute;
-            border-radius: 50%;
-        }
+.background .shape {
+    height: 200px;
+    width: 200px;
+    position: absolute;
+    border-radius: 50%;
+}
 
-    .shape:first-child {
-        background: linear-gradient(#1845ad, #23a2f6);
-        left: -80px;
-        top: -80px;
-    }
+.shape:first-child {
+    background: linear-gradient(#1845ad, #23a2f6);
+    left: -80px;
+    top: -80px;
+}
 
-    .shape:last-child {
-        background: linear-gradient(to right, #ff512f, #f09819);
-        right: -30px;
-        bottom: -80px;
-    }
+.shape:last-child {
+    background: linear-gradient(to right, #ff512f, #f09819);
+    right: -30px;
+    bottom: -80px;
+}
 
-    form {
-        height: 450px; 
-        width: 400px;
-        background-color: rgba(255, 255, 255, 0.13);
-        position: absolute;
-        transform: translate(-50%, -50%);
-        top: 50%;
-        left: 50%;
-        border-radius: 10px;
-        backdrop-filter: blur(10px);
-        border: 2px solid rgba(255, 255, 255, 0.1);
-        box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
-        padding: 50px 35px;
-    }
+form {
+    height: 450px;
+    width: 400px;
+    background-color: rgba(255, 255, 255, 0.13);
+    position: absolute;
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+    border-radius: 10px;
+    backdrop-filter: blur(10px);
+    border: 2px solid rgba(255, 255, 255, 0.1);
+    box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+    padding: 50px 35px;
+}
 
-        form h3 {
-            font-size: 32px;
-            font-weight: 500;
-            line-height: 42px;
-            text-align: center;
-            margin-bottom: 30px;
-        }
+form h3 {
+    font-size: 32px;
+    font-weight: 500;
+    line-height: 42px;
+    text-align: center;
+    margin-bottom: 30px;
+}
 
-    label {
-        display: block;
-        font-size: 16px;
-        font-weight: 500;
-        margin-bottom: 10px;
-    }
+label {
+    display: block;
+    font-size: 16px;
+    font-weight: 500;
+    margin-bottom: 10px;
+}
 
-    input[type="text"],
-    input[type="email"],
-    input[type="password"] {
-        height: 50px;
-        width: 100%;
-        background-color: rgba(255, 255, 255, 0.07);
-        border-radius: 3px;
-        padding: 0 10px;
-        margin-bottom: 20px;
-        font-size: 14px;
-        font-weight: 300;
-        color: #000000; 
-    }
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+    height: 50px;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.07);
+    border-radius: 3px;
+    padding: 0 10px;
+    margin-bottom: 20px;
+    font-size: 14px;
+    font-weight: 300;
+    color: #000000;
+}
 
-    ::placeholder {
-        color: #e5e5e5;
-    }
+::placeholder {
+    color: #e5e5e5;
+}
 
-    button {
-        width: 100%;
-        background-color: #808080;
-        color: #ffffff; 
-        padding: 15px 0;
-        font-size: 18px;
-        font-weight: 600;
-        border-radius: 5px;
-        cursor: pointer;
-        border: none;
-        margin-top: 20px;
-    }
+button {
+    width: 100%;
+    background-color: #808080;
+    color: #ffffff;
+    padding: 15px 0;
+    font-size: 18px;
+    font-weight: 600;
+    border-radius: 5px;
+    cursor: pointer;
+    border: none;
+    margin-top: 20px;
+}
 
-        button:hover {
-            background-color: #a9a9a9; 
-        }
+button:hover {
+    background-color: #a9a9a9;
+}
 
-    .social {
-        margin-top: 30px;
-        display: flex;
-        justify-content: center;
-    }
+.social {
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+}
 
-        .social div {
-            background: red;
-            width: 150px;
-            border-radius: 3px;
-            padding: 5px 10px 10px 5px;
-            background-color: rgba(255, 255, 255, 0.27);
-            color: #eaf0fb;
-            text-align: center;
-            margin: 0 10px;
-        }
+.social div {
+    background: red;
+    width: 150px;
+    border-radius: 3px;
+    padding: 5px 10px 10px 5px;
+    background-color: rgba(255, 255, 255, 0.27);
+    color: #eaf0fb;
+    text-align: center;
+    margin: 0 10px;
+}
 
-            .social div:hover {
-                background-color: rgba(255, 255, 255, 0.47);
-            }
+.social div:hover {
+    background-color: rgba(255, 255, 255, 0.47);
+}
 
-        .social i {
-            margin-right: 4px;
-        }
+.social i {
+    margin-right: 4px;
+}
 
-    input[type="text"],
-    input[type="password"] {
-        height: 50px;
-        width: 100%;
-        background-color: rgba(255, 255, 255, 0.07);
-        border-radius: 3px;
-        padding: 0 10px;
-        margin-bottom: 20px;
-        font-size: 14px;
-        font-weight: 300;
-        color: #000000;
-    }
-
-
-
+input[type="text"],
+input[type="password"] {
+    height: 50px;
+    width: 100%;
+    background-color: rgba(255, 255, 255, 0.07);
+    border-radius: 3px;
+    padding: 0 10px;
+    margin-bottom: 20px;
+    font-size: 14px;
+    font-weight: 300;
+    color: #000000;
+}
 </style>
