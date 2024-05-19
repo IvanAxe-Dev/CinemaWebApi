d2aec31879ddbe8a86b3706cb9663e220f95f9f1
Dmytro <boikodimaa17@gmail.com>
2024-05-19 23:20:46 +0300

    utils

--- Cinema.WebClient/cinema-client/src/utils.js
+++ Cinema.WebClient/cinema-client/src/utils.js
@@ -7,18 +7,18 @@ export function formatDate(dateString) {
 
 export function getDayMonth(dateString) {
     const date = new Date(dateString);
-    const day = date.getDate(); // виправлено getDay() на getDate()
-    const month = date.toLocaleString('en', { month: 'long' });
+    const day = date.getDay();
+    const month = date.toLocaleString('en', { month: 'long'});
     return `${day} ${month}`;
 }
 
 export function getAverageRating(ratingArray) {
-    if (ratingArray.length === 0) return 0; // виправлено умову
+    if (!ratingArray.length > 0) return 0;
     const sum = ratingArray.reduce((total, current) => total + current, 0);
-    return Math.round(sum / ratingArray.length);
+    return Math.round((sum / ratingArray.length));
 }
 
-export function formatPoster(imageSrc, width = 810, height = 1200) {
+export function formatPoster(imageSrc, width=810, height=1200) {
     const imageUrlRegex = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/g;
     const isValidImageUrl = imageUrlRegex.test(imageSrc);
     const posterPlaceholder = `https://via.placeholder.com/${width}x${height}`;
@@ -31,14 +31,14 @@ export function formatPoster(imageSrc, width = 810, height = 1200) {
 }
 
 export function validateTrailerSrc(videoSrc) {
-    const youtubeVideoRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/;
+    const youtubeVideoRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/;
     return youtubeVideoRegex.test(videoSrc);
 }
 
 // Divides sessions on the same day into arrays
 function divideSessionsByDate(sessions) {
     return sessions.reduce((accumulator, session) => {
-        const date = dayjs(session.date).format('YYYY-MM-DD'); // форматування дати
+        const date = dayjs(session.date);
 
         if (!accumulator[date]) accumulator[date] = [];
 
@@ -51,9 +51,9 @@ function divideSessionsByDate(sessions) {
 // Sorts sessions by date in ascending order while replacing headers with 'M D'
 function formatDateHeaders(sessions) {
     sessions = Object.entries(sessions)
-        .sort((a, b) => new Date(a[0]) - new Date(b[0])) // змінено сортування
-        .map(([date, sessions]) => ({ date, sessions }));
-
+    .sort((a, b) => new Date(b[0]) - new Date(a[0]))
+    .map(([date, sessions]) => ({ date, sessions }));
+  
     for (let i = 0; i < sessions.length; i++) {
         sessions[i].date = getDayMonth(sessions[i].date);
     }
@@ -68,13 +68,13 @@ export function formatSessionsData(sessions) {
 }
 
 export function validateLoginData(userData) {
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
-    const usernameValid = userData.emailOrUsername.length <= 50 && userData.emailOrUsername.length >= 5;
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+    const usernameValid = userData.emailOrUsername.length < 50 && userData.emailOrUsername.length > 5;
     const passwordValid = passwordRegex.test(userData.password);
-
-    let status = { invalid: false };
-    if (!passwordValid) status.password = true, status.invalid = true;
-    if (!usernameValid) status.username = true, status.invalid = true;
+    
+    let status = {invalid: new Boolean(false)};
+    if (!passwordValid) status.password = new Boolean(true), status.invalid = new Boolean(true);
+    if (!usernameValid) status.username = new Boolean(true), status.invalid = new Boolean(true);
 
     return status;
 }
