68b2c59dd8484e14fea576c5f1d30daa9f4d7cac
Карпенко Глеб <glebkarpenko1@gmail.com>
2024-05-20 16:59:51 +0300

    update store mutations

--- Cinema.WebClient/cinema-client/src/store/index.js
+++ Cinema.WebClient/cinema-client/src/store/index.js
@@ -2,23 +2,38 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    isLoggedIn: localStorage.getItem('isLoggedIn') === 'true'
+    isLoggedIn: localStorage.getItem('isLoggedIn') === 'true',
+    authorizationToken: localStorage.getItem('authorizationToken') === '',
+    userEmail: localStorage.getItem('userEmail') === ''
   },
   getters: {
-    isLoggedIn: state => state.isLoggedIn
+    isLoggedIn: state => state.isLoggedIn,
+    authorizationToken: state => state.authorizationToken,
+    userEmail: state => state.userEmail,
   },
   mutations: {
     setLoggedIn(state, isLoggedIn) {
-      state.isLoggedIn = isLoggedIn
+      state.isLoggedIn = isLoggedIn;
       localStorage.setItem('isLoggedIn', isLoggedIn);
+    },
+    setAuthorizationToken(state, authorizationToken) {
+      state.authorizationToken = authorizationToken;
+      localStorage.setItem('authorizationToken', authorizationToken);
+    },
+    setUserEmail(state, userEmail) {
+      state.userEmail = userEmail;
+      localStorage.setItem('userEmail', userEmail);
     }
   },
   actions: {
-    authorize({ commit }) {
+    authorize({ commit }, tokenValue, emailValue) {
       commit('setLoggedIn', true)
+      commit('setAuthorizationToken', tokenValue)
+      commit('setUserEmail', emailValue)
     },
     logout({ commit }) {
       commit('setLoggedIn', false)
+      commit('setAuthorizationToken', '')
     }
   }
 })
